4a6f8fb1251d2dc732a7752343667db037486be8
firminm <matthewjf1@gmail.com>
2022-09-13 12:20:38 -0400

    initial setup

--- backend/models/task.js
+++ backend/models/task.js
@@ -2,5 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 /* CREATE 'taskSchema' */
+/**
+ * @param {String} body The body
+ */
+const taskSchema = new mongoose.Schema(body,
+    {
+        methods: {
+            find() { return -1; }
+        }
+    }
+    );
 
 module.exports = mongoose.model("task", taskSchema);
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/create", async (req, res) => {
+  console.log(req.body)
   try {
     const task = await new Task(req.body).save();
     res.send(task);
@@ -20,8 +21,39 @@ router.get("/", async (req, res) => {
   }
 });
 
-/* CREATE 'PUT' REQUEST */
+/**
+ * Reminder....
+ * GET  = parameters for function stored in URL
+ * POST = parameters for function stored in body
+ */
 
-/* CREATE 'DELETE' REQUEST */
+
+
+/**
+ * CREATE 'PUT' REQUEST
+ * Finds the specified document and updates the value present in the body
+ * Alternatively - deletes document and replaces it with new one
+ */
+router.post("/put", async (req, res) => {
+  try {
+    const task = await new Task(req.body).save();
+    res.send(task);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+/** 
+ * CREATE 'DELETE' REQUEST 
+ * Deletes the document with the ID (or matching content?) specified in URL
+*/
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.send(tasks);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 module.exports = router;
